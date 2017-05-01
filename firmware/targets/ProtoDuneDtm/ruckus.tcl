9a8b8e9f900f45de5c1eab9d6f8b97bd107540bd
Ryan Herbst <rherbst@slac.stanford.edu>
2017-05-01 10:57:25 -0700

    Undo merge.

--- firmware/targets/ProtoDuneDtm/ruckus.tcl
+++ firmware/targets/ProtoDuneDtm/ruckus.tcl
@@ -4,8 +4,12 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load common and sub-module ruckus.tcl files
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/surf
 loadRuckusTcl $::env(PROJ_DIR)/../../submodules/rce-gen3-fw-lib/DtmCore
-loadRuckusTcl $::env(PROJ_DIR)/../../submodules/proto-dune-ep-timing-core
 loadRuckusTcl $::env(PROJ_DIR)/../../common/dtm
+
+# Load the CERN Timing Library
+loadSource -dir $::env(PROJ_DIR)/../../submodules/timing-board-firmware/components/pdts/firmware/hdl
+loadSource -dir $::env(PROJ_DIR)/../../submodules/timing-board-firmware/projects/endpoint/firmware/hdl
+remove_files [get_files {pdts_endpoint_dummy.vhd}] 
 
 # Load local Source Code and constraints
 loadSource -dir       "$::DIR_PATH/hdl/"
