e4729936492388598703dd959bdc0b172a5d011c
Larry Ruckman <ruckman@slac.stanford.edu>
2017-06-19 14:24:17 -0700

    cleanup

--- firmware/common/dpm/rtl/DummyDataGenerator.py
+++ firmware/common/dpm/rtl/DummyDataGenerator.py
@@ -1,3 +1,12 @@
+##############################################################################
+## This file is part of 'DUNE Data compression'.
+## It is subject to the license terms in the LICENSE.txt file found in the 
+## top-level directory of this distribution and at: 
+##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html. 
+## No part of 'DUNE Data compression', including this file, 
+## may be copied, modified, propagated, or distributed except according to 
+## the terms contained in the LICENSE.txt file.
+##############################################################################
 
 def PrintValue(x,y):            
     print( 'Word[%i]:\t0x%016X'%(x,y) )
