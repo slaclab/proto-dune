ca1d052a7726b70ef8fb85affdb894036c7c06fd
Ryan Herbst <rherbst@slac.stanford.edu>
2018-07-05 20:20:24 -0700

    Added verilog files

---
+++ firmware/common/VivadoHLS/DuneDataCompression/vivado_hls/verilog/DuneDataCompressionCore.result.rb
@@ -0,0 +1,14 @@
+$Footmark = "FPGA_Xilinx"
+$Description = "by Vivado"
+
+
+#=== Resource usage ===
+$SLICE = "0"
+$LUT = "38191"
+$FF = "23335"
+$DSP = "8"
+$BRAM ="290"
+$SRL ="103"
+#=== Final timing ===
+$TargetCP = "8.000"
+$CP = "7.686"
